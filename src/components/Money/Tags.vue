83dc46a3b1bdd66756926c4bd714b15667e1435a
oldDong <2232890213@qq.com>
2020-09-10 22:30:34 +0800

    完成了标签的添加

--- src/components/Money/Tags.vue
+++ src/components/Money/Tags.vue
@@ -2,20 +2,43 @@
   <!--衣食住行-->
   <div class="tags">
     <div class="new">
-      <button>新增标签</button>
+      <button @click="create">新增标签</button>
     </div>
     <ul class="current">
-      <li>衣</li>
-      <li>食</li>
-      <li>住</li>
-      <li>行</li>
+      <li v-for="tag in dataSource" :key="tag"
+          :class="{selected:selectedTags.indexOf(tag)>=0}"
+        @click="toggle(tag)">{{tag}}</li>
+
     </ul>
   </div>
 </template>
 
-<script>
-export default {
-name: "Tags"
+<script lang="ts">
+import Vue from 'vue'
+import {Component, Prop} from 'vue-property-decorator';
+@Component
+export default class Tags extends Vue{
+  @Prop() dataSource: string[] | undefined;
+  //选中的标签数组
+  selectedTags: string[]=[];
+  toggle(tag: string){
+    const index = this.selectedTags.indexOf(tag);
+    if(index>=0){
+      this.selectedTags.splice(index,1);
+    }else {
+      this.selectedTags.push(tag)
+    }
+  }
+//  新增标签
+  create(){
+    const name=prompt("请输入标签名：")
+    if(name === ''){
+      alert("不能输入空的标签名");
+    }else if(this.dataSource){
+      this.$emit('update:dataSource',
+          [...this.dataSource,name]);
+    }
+  }
 }
 </script>
 
@@ -33,12 +56,17 @@ name: "Tags"
     flex-wrap: wrap;
 
     > li{
-      background:#d9d9d9;
+      $bg:#d9d9d9;
+      background:$bg;
       line-height: 24px;
       border-radius:(24px/2) ;
       padding: 0 16px;
       margin-right: 12px;
       margin-top:4px;
+      &.selected{
+        background: darken($bg,30%);
+        color: white;
+      }
     }
   }
   > .new {
--- src/views/Money.vue
+++ src/views/Money.vue
@@ -7,12 +7,12 @@
 <!--备注-->
       <Notes />
 <!--衣食住行-->
-      <Tags />
+      <Tags :data-source.sync="tags"/>
 
     </Layout>
 </template>
 
-<script lang="ts">
+<script lang="js">
 import NumberPad from '@/components/Money/NumberPad.vue';
 import Types from '@/components/Money/Types.vue';
 import Notes from '@/components/Money/Notes.vue';
@@ -21,6 +21,11 @@ import Tags from '@/components/Money/Tags.vue';
 export default {
   name: "Money",
   components: {Tags, Notes, Types, NumberPad},
+  data(){
+    return{
+      tags:['衣','食','住','行']
+    }
+  }
 }
 </script>
 
