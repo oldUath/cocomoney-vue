dba9028b68dbba05b97cdf537e33721dd13a6b91
oldDong <2232890213@qq.com>
2020-09-13 11:45:28 +0800

    添加数据到localStorage

--- src/components/Money/NumberPad.vue
+++ src/components/Money/NumberPad.vue
@@ -64,6 +64,8 @@ export default class NumberPad extends Vue {
 //  ok
   ok(){
     this.$emit('update:value',this.output)
+    this.$emit('submit',this.output)
+    this.output = '0'
   }
 }
 </script>
--- src/views/Money.vue
+++ src/views/Money.vue
@@ -2,14 +2,14 @@
 
     <Layout class-prefix="layout">
 <!--      数字输入面板-->
-     <NumberPad @update:value="onUpdateAmount"/>
+     <NumberPad @update:value="onUpdateAmount" @submit="saveRecord" />
 <!--支出收入按钮-->
       <Types :value="record.type" @update:value="onUpdateType"/>
 <!--备注-->
       <Notes @update:value="onUpdateNotes" />
 <!--衣食住行-->
       <Tags :data-source.sync="tags" @update:value="onUpdateTags"/>
-      {{record}}
+{{recordList}}
     </Layout>
 </template>
 
@@ -19,7 +19,9 @@ import NumberPad from '@/components/Money/NumberPad.vue';
 import Types from '@/components/Money/Types.vue';
 import Notes from '@/components/Money/Notes.vue';
 import Tags from '@/components/Money/Tags.vue';
-import {Component} from 'vue-property-decorator';
+import {Component, Watch} from 'vue-property-decorator';
+
+window.localStorage.setItem('version','0.0.1');
 
 //定义一种类型
 type Record = {
@@ -27,12 +29,14 @@ type Record = {
   notes: string;
   type: string;
   amount: number;
+  createdAt?: Date;
 }
 @Component({
   components: {Tags, Notes, Types, NumberPad}
 })
 export default class Money extends  Vue{
   tags=['衣','食','住','行'];
+  recordList: Record[]=JSON.parse(window.localStorage.getItem('recordList') || '[]');
   //数据类型初始值，记录Money页面的数据
   record: Record={
     tags:[],notes:'',type:'-',amount:0
@@ -51,6 +55,17 @@ export default class Money extends  Vue{
   onUpdateAmount(value: string){
     this.record.amount=parseFloat(value)
 
+  }
+  saveRecord(){
+    //进行深拷贝,如果直接把record给push他们的值永远是一样的，因为push的是地址
+    const record2=JSON.parse(JSON.stringify(this.record));
+    record2.createAt=new Date();
+    this.recordList.push(record2)
+    console.log(this.recordList);
+  }
+@Watch('recordList')
+  onRecordListChange(){
+    window.localStorage.setItem('recordList',JSON.stringify(this.recordList))
   }
 
 }
