e32c29e4a4398f3879bfb83c4b89cd690bc63818
oldDong <2232890213@qq.com>
2020-09-20 16:53:53 +0800

    新增Label的编辑页面

--- src/views/Labels.vue
+++ src/views/Labels.vue
@@ -1,12 +1,10 @@
 <template>
   <Layout>
     <ol class="tags">
-
       <li v-for="tag in tags" :key="tag.id">
         <span>{{tag.name}}</span>
         <Icon name="right"/>
       </li>
-
     </ol>
     <div class="createTag-wrapper">
       <button class="createTag" @click="createTag">新建标签</button>
