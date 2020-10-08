b7a5140a7710116f49fc456817cd0733bd870608
oldDong <2232890213@qq.com>
2020-10-08 13:17:59 +0800

    重新封装recordListModel

--- src/lib/createId.ts
+++ src/lib/createId.ts
@@ -2,7 +2,8 @@
 let id: number = parseInt(window.localStorage.getItem('_idMax') || '0' ) || 0;
 
 function createId(){
-    id++
+    id++;
+    window.localStorage.setItem('_idMax',id.toString());
     return id;
 }
 export default createId
--- src/models/recordListModel.ts
+++ src/models/recordListModel.ts
@@ -1,13 +1,17 @@
 const localStorageKeyName = 'recordList';
 const recordListModel = {
+    //强制的类型断言，使data是一个RecordItem类型的数组
+   data:[] as RecordItem[] ,
     clone(data: RecordItem[] | RecordItem){
         return JSON.parse(JSON.stringify(data));
     },
     fetch() {
-        return JSON.parse(window.localStorage.getItem('recordList') || '[]') as RecordItem[] ;
+        this.data = JSON.parse(window.localStorage.getItem('recordList') || '[]') as RecordItem[] ;
+        return this.data;
     },
-    save(data: RecordItem[]) {
-        window.localStorage.setItem(localStorageKeyName, JSON.stringify(data));
+    save() {
+        window.localStorage.setItem(localStorageKeyName,
+            JSON.stringify(this.data));
     }
 
 };
