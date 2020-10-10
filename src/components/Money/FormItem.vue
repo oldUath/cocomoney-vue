fef4585fd4b3e308555ba3be779b9f992a320964
oldDong <2232890213@qq.com>
2020-10-10 17:19:23 +0800

    删除，修复刷新变成404的bug

--- src/components/Money/FormItem.vue
+++ src/components/Money/FormItem.vue
@@ -21,7 +21,6 @@ export  default class FormItem extends Vue{
   @Prop({required:true}) fieldName!: string;
   @Prop() placeholder?: string;
 
-  @Watch('value')
   onValueChanged(value: string){
     this.$emit('update:value',value)
   }
--- src/views/EditLabel.vue
+++ src/views/EditLabel.vue
@@ -34,6 +34,7 @@ export default class EditerLabel extends Vue {
   }
   created() {
     const id = this.$route.params.id;
+    this.$store.commit('fetchTags')
     this.$store.commit('setCurrentTag',id);
     if (!this.tag){
       this.$router.replace('/404');
@@ -42,18 +43,14 @@ export default class EditerLabel extends Vue {
 
   update(name: string) {
     if (this.tag) {
-      //TODO
-      // store.updateTag(this.tag.id, name);
+      this.$store.commit('updateTag', {id:this.tag.id,name});
     }
   }
 
   remove() {
     if (this.tag) {
-      return [];
-      //TODO
-      // if (store.removeTag(this.tag.id)) {
-      //   this.$router.back();
-      // }
+      this.$store.commit('removeTag',this.tag.id);
+
     }
   }
 
